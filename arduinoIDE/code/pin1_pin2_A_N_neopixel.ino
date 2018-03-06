5c1b08a07545024a8e6a238c49cf3fa3074513d1
Lars Gimse <lagi@iktsenteret.no>
2018-03-06 14:57:42 +0100

    Create pin1_pin2_A_N_neopixel.ino

---
+++ arduinoIDE/code/pin1_pin2_A_N_neopixel.ino
@@ -0,0 +1,141 @@
+#include <Adafruit_NeoPixel.h>
+
+#define PIN 0
+#define NUM_LEDS 72
+
+const int buttonA = 5;     // the number of the pushbutton pin
+const int buttonB = 11;     // the number of the pushbutton pin
+
+int digitPins[] = {1,2,3,4,6,7,8,9,10,12,13,14,15,16,19,20};
+int pinCount = 16;
+
+
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LEDS, PIN, NEO_GRBW + NEO_KHZ800);
+ 
+void setup() {  
+  Serial.begin(9600);
+  
+  Serial.println("microbit is ready!");
+  
+  pinMode(buttonA, INPUT);  
+  pinMode(buttonB, INPUT);
+
+  for (int thisPin = 0; thisPin < pinCount; thisPin++) {
+    pinMode(digitPins[thisPin], INPUT);
+  }
+  
+  strip.begin();
+  strip.show();  
+}
+ 
+void loop(){
+  if (! digitalRead(buttonA)) {
+    Serial.println("Button A pressed");
+    //colorWipe(strip.Color(255, 0, 0), 50);
+    meteorRain(0, 0, 255,2, 128, true, 30);
+  }
+  if (! digitalRead(buttonB)) {
+    Serial.println("Button B pressed");
+    //colorWipe(strip.Color(0, 255, 0), 50);
+    meteorRain(0, 255, 0,2, 128, true, 30);
+  }
+  if (! digitalRead(1)) {
+    Serial.println("pin1");
+    colorWipe(strip.Color(255, 0, 0), 10);
+    colorWipe(strip.Color(0, 0, 0), 5);
+  }
+  if (! digitalRead(2)) {
+    Serial.println("pin2");
+    colorWipe(strip.Color(0, 255, 0), 10);
+    colorWipe(strip.Color(0, 0, 0), 5);
+  }
+  delay(10);
+}
+
+void colorWipe(uint32_t c, uint8_t wait) {
+  for(uint16_t i=0; i<strip.numPixels(); i++) {
+    strip.setPixelColor(i, c);
+    strip.show();
+    delay(wait);
+  }
+}
+
+void meteorRain(byte red, byte green, byte blue, byte meteorSize, byte meteorTrailDecay, boolean meteorRandomDecay, int SpeedDelay) {  
+  setAll(0,0,0);
+  
+  for(int i = 0; i < NUM_LEDS+NUM_LEDS; i++) {
+    
+    
+    // fade brightness all LEDs one step
+    for(int j=0; j<NUM_LEDS; j++) {
+      if( (!meteorRandomDecay) || (random(10)>5) ) {
+        fadeToBlack(j, meteorTrailDecay );        
+      }
+    }
+    
+    // draw meteor
+    for(int j = 0; j < meteorSize; j++) {
+      if( ( i-j <NUM_LEDS) && (i-j>=0) ) {
+        setPixel(i-j, red, green, blue);
+      } 
+    }
+   
+    showStrip();
+    delay(SpeedDelay);
+  }
+}
+
+void fadeToBlack(int ledNo, byte fadeValue) {
+ #ifdef ADAFRUIT_NEOPIXEL_H 
+    // NeoPixel
+    uint32_t oldColor;
+    uint8_t r, g, b;
+    int value;
+    
+    oldColor = strip.getPixelColor(ledNo);
+    r = (oldColor & 0x00ff0000UL) >> 16;
+    g = (oldColor & 0x0000ff00UL) >> 8;
+    b = (oldColor & 0x000000ffUL);
+
+    r=(r<=10)? 0 : (int) r-(r*fadeValue/256);
+    g=(g<=10)? 0 : (int) g-(g*fadeValue/256);
+    b=(b<=10)? 0 : (int) b-(b*fadeValue/256);
+    
+    strip.setPixelColor(ledNo, r,g,b);
+ #endif
+ #ifndef ADAFRUIT_NEOPIXEL_H
+   // FastLED
+   leds[ledNo].fadeToBlackBy( fadeValue );
+ #endif  
+}
+
+void showStrip() {
+ #ifdef ADAFRUIT_NEOPIXEL_H 
+   // NeoPixel
+   strip.show();
+ #endif
+ #ifndef ADAFRUIT_NEOPIXEL_H
+   // FastLED
+   FastLED.show();
+ #endif
+}
+
+void setPixel(int Pixel, byte red, byte green, byte blue) {
+ #ifdef ADAFRUIT_NEOPIXEL_H 
+   // NeoPixel
+   strip.setPixelColor(Pixel, strip.Color(red, green, blue));
+ #endif
+ #ifndef ADAFRUIT_NEOPIXEL_H 
+   // FastLED
+   leds[Pixel].r = red;
+   leds[Pixel].g = green;
+   leds[Pixel].b = blue;
+ #endif
+}
+
+void setAll(byte red, byte green, byte blue) {
+  for(int i = 0; i < NUM_LEDS; i++ ) {
+    setPixel(i, red, green, blue); 
+  }
+  showStrip();
+}
